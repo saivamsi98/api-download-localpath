41fe078fc96b25bdacd209c45984efd9d9595aba
saivamsi98 <67453166+saivamsi98@users.noreply.github.com>
2020-06-27 23:01:59 +0530

    Update header.h

--- header.h
+++ header.h
@@ -5,3 +5,8 @@
 #include<stdarg.h>
 #include<LPC214X.H>  //using ARM controller fot the task
 #include<curl/curl.h>//this is the header file to download from url that contains all the functions for this program
+
+
+
+//for this task we need to interface lpc2148 to pc by rs232 terminal
+//we need to debug it by using keil compiler
