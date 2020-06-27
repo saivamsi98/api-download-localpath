a588c7100c35d31c876ed2fa8ff8303a426c88f1
saivamsi98 <67453166+saivamsi98@users.noreply.github.com>
2020-06-27 23:24:53 +0530

    Update header.h

--- header.h
+++ header.h
@@ -5,6 +5,9 @@
 #include<stdarg.h>
 #include<LPC214X.H>  //using ARM controller fot the task
 #include<curl/curl.h>//this is the header file to download from url that contains all the functions for this program
+extern void io_write(int port,int pin,int value);
+extern void io_dir(int port,int pin,int direction);
+extern int io_read(int port,int pin);
 
 
 
