af6057f0e3a730ce73df24a212e93617f0df9f65
saivamsi98 <67453166+saivamsi98@users.noreply.github.com>
2020-06-27 22:07:54 +0530

    Create header.h

---
+++ header.h
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include"time.h"    //for clock
+#include<stdlib.h>
+#include<stdarg.h>
+#include<LPC214X.H>  //using ARM controller fot the task
+#include<curl/curl.h>//this is the header file to download from url that contains all the functions for this program
